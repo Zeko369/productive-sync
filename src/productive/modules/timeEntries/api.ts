4f005684ed943c93284dddba6486ec844c1e1ebb
Fran Zekan <zekan.fran369@gmail.com>
2020-07-26 21:55:11 +0200

    Productive TimeEntries list and pass date to create

--- src/productive/modules/timeEntries/api.ts
+++ src/productive/modules/timeEntries/api.ts
@@ -1,48 +1,44 @@
-import { post } from '../../../lib/http';
 import { URLSearchParams } from 'url';
+import { post, get } from '../../../lib/http';
+import { TimeEntry, TimeEntries } from './types';
+import { pDate } from '../../../helpers/date';
 
 export const create = async (
-  text: string,
+  note: string,
   serviceId: string,
   personId: string,
-  time: number
-): Promise<any> => {
+  time: number,
+  date: Date
+): Promise<TimeEntry> => {
   const params = new URLSearchParams();
-  const date = new Date().toISOString().slice(0, 10);
-
-  params.append('date', date);
+  params.append('date', pDate(date));
 
   const body = {
     data: {
       type: 'time_entries',
-      attributes: {
-        note: text,
-        date,
-        time
-      },
+      attributes: { note, date, time },
       relationships: {
-        person: {
-          data: {
-            type: 'people',
-            id: personId
-          }
-        },
-        service: {
-          data: {
-            type: 'services',
-            id: serviceId
-          }
-        }
-        // task: {
-        //   data: {
-        //     type: 'tasks',
-        //     id: '110'
-        //   }
-        // }
+        person: { data: { type: 'people', id: personId } },
+        service: { data: { type: 'services', id: serviceId } }
       }
     }
   };
 
   const res = await post({ url: '/time_entries', params }, body);
   return res.data;
+};
+
+export const list = async (
+  personId: string,
+  after: Date,
+  before = new Date()
+): Promise<TimeEntries> => {
+  const params = new URLSearchParams();
+  params.append('filter[person_id]', personId);
+  params.append('filter[after]', pDate(after));
+  params.append('filter[before]', pDate(before));
+  params.append('sort', 'date');
+
+  const res = await get({ url: '/time_entries', params });
+  return res.data as TimeEntries;
 };
--- src/productive/modules/timeEntries/types.ts
+++ src/productive/modules/timeEntries/types.ts
@@ -1,1 +1,14 @@
-export {};
+export interface TimeEntry {
+  id: string;
+  type: 'time_entries';
+  attributes: {
+    date: string;
+    time: number;
+    note: string;
+  };
+  relationships: any;
+}
+
+export interface TimeEntries {
+  data: TimeEntry[];
+}
