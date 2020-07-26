504674b5d2ab6241a9ebdfbcba61a32a7ee88b88
Fran Zekan <zekan.fran369@gmail.com>
2020-07-26 21:55:42 +0200

    Whole refactor of index, basically working

--- src/index.ts
+++ src/index.ts
@@ -5,38 +5,89 @@ import api from './productive';
 import { getEvents } from './gcal/lib';
 import { map, personId } from './data';
 import template from './template';
-import { stat } from 'fs';
+import { toBegin, toEnd } from './helpers/date';
+import { MIN_DATE } from './config';
+import { TimeEntries, TimeEntry } from './productive/modules/timeEntries';
+
+const findTe = (timeEntries: TimeEntries, id: string) => {
+  return timeEntries.data.find((te) => {
+    const res = te.attributes.note.match(/#[a-z0-9A-Z]*#/);
+    if (res) {
+      const noteId = res[0].slice(1, -1);
+      return noteId === id;
+    }
+  });
+};
+
+const compareEntry = (entry: TimeEntry, data: { minutes: number; time: string }) => {
+  console.log(entry.attributes.note);
+  if (entry.attributes.time !== data.minutes) {
+    return false;
+  }
+
+  // TODO: Compare note
+
+  return true;
+};
+
+const days = [24, 25, 26];
+
+const genTime = (startTime?: string | null, endTime?: string | null) => {
+  const start = new Date(startTime || Date.now());
+  const end = new Date(endTime || Date.now());
+  const minutes = (end.getTime() - start.getTime()) / 1000 / 60;
+  const time = `${start.toTimeString().slice(0, 5)} - ${end.toTimeString().slice(0, 5)}`;
+
+  return { minutes, time };
+};
 
 (async () => {
   try {
     if (!process.env.GCAL_ID) throw new Error('GCAL_ID missing');
 
-    const events = await getEvents(process.env.GCAL_ID);
+    for (let dayId of days) {
+      const day = new Date(2020, 6, dayId + 1);
 
-    const promises = (events || []).map((event) => {
-      const { id, summary, start: startTime, end: endTime } = event;
+      if (day < MIN_DATE) {
+        throw new Error('Could break, old date');
+      }
 
-      if (!id || !summary) throw new Error('ID missing');
+      const timeEntries = await api.timeEntries.list(personId, toBegin(day), toEnd(day));
+      const events = await getEvents(process.env.GCAL_ID, day);
 
-      const start = new Date(startTime?.dateTime || '');
-      const end = new Date(endTime?.dateTime || '');
-      const minutes = (end.getTime() - start.getTime()) / 1000 / 60;
+      if (!events) throw new Error('Error loading events');
 
-      const titles = summary.split(':');
-      if (titles.length < 2) throw new Error('Cant find slug');
-      const slug = titles[0].trim();
-      const title = titles.slice(1).join(':');
-      const time = `${start.toTimeString().slice(0, 5)} - ${end.toTimeString().slice(0, 5)}`;
+      for (let event of events) {
+        const { id, summary, start: startTime, end: endTime } = event;
+        if (!id || !summary) throw new Error('ID missing');
 
-      const project = map.get(slug);
-      if (!project) throw new Error('Cant find project with that slug');
+        const { time, minutes } = genTime(startTime?.date, endTime?.date);
 
-      const note = template({ title, time, id });
+        const titles = summary.split(':');
+        if (titles.length < 2) throw new Error(`Cant find slug ${summary}`);
+        const slug = titles[0].trim();
+        const title = titles.slice(1).join(':');
 
-      return api.timeEntries.create(note, project.sid, personId, minutes);
-    });
+        const project = map.get(slug);
+        if (!project) throw new Error('Cant find project with that slug');
 
-    await Promise.all(promises);
+        const note = template({ title, time, id });
+
+        const currentEntry = findTe(timeEntries, id || '');
+        if (currentEntry) {
+          console.log('Found entry');
+
+          if (!compareEntry(currentEntry, { minutes, time })) {
+            console.error('Update entry');
+          }
+
+          continue;
+        }
+
+        await api.timeEntries.create(note, project.sid, personId, minutes, day);
+        // console.log(note, project.sid, personId, minutes, day);
+      }
+    }
   } catch (err) {
     console.log(err);
   }
