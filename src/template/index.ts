4e3386abeab8f14273520d280453a93801b2d9fa
Fran Zekan <zekan.fran369@gmail.com>
2020-07-26 20:12:40 +0200

    Add template

---
+++ src/template/index.ts
@@ -0,0 +1,15 @@
+import { renderFile } from 'ejs';
+import { join } from 'path';
+
+interface Props {
+  title: string;
+  time: string;
+  id: string;
+}
+
+const template = (props: Props): string => {
+  const { id, title, time } = props;
+  return `${title} - <b>${time}</b><br />${id}`;
+};
+
+export default template;
