f14d262c5b2a1e159131e63b559815dab4ad37c6
Fran Zekan <zekan.fran369@gmail.com>
2020-07-26 20:47:32 +0200

    Get stuff working

--- src/index.ts
+++ src/index.ts
@@ -2,50 +2,41 @@ import { config } from 'dotenv';
 config();
 
 import api from './productive';
+import { getEvents } from './gcal/lib';
 import { map, personId } from './data';
 import template from './template';
+import { stat } from 'fs';
 
 (async () => {
   try {
-    const note = template({
-      title: 'Fixing templates',
-      time: '4h',
-      id: '#asd'
+    if (!process.env.GCAL_ID) throw new Error('GCAL_ID missing');
+
+    const events = await getEvents(process.env.GCAL_ID);
+
+    const promises = (events || []).map((event) => {
+      const { id, summary, start: startTime, end: endTime } = event;
+
+      if (!id || !summary) throw new Error('ID missing');
+
+      const start = new Date(startTime?.dateTime || '');
+      const end = new Date(endTime?.dateTime || '');
+      const minutes = (end.getTime() - start.getTime()) / 1000 / 60;
+
+      const titles = summary.split(':');
+      if (titles.length < 2) throw new Error('Cant find slug');
+      const slug = titles[0].trim();
+      const title = titles.slice(1).join(':');
+      const time = `${start.toTimeString().slice(0, 5)} - ${end.toTimeString().slice(0, 5)}`;
+
+      const project = map.get(slug);
+      if (!project) throw new Error('Cant find project with that slug');
+
+      const note = template({ title, time, id });
+
+      return api.timeEntries.create(note, project.sid, personId, minutes);
     });
 
-    const res = await api.timeEntries.create(note, '97473', '40843', 60);
-    /* Get ids for map*/
-    // const res = await projects.list();
-    // const proj = res.data.map((p) => ({ id: p.id, name: p.attributes.name }));
-    // for (let p of proj) {
-    //   const { id, name } = p;
-    //   if (
-    //     Object.values(map)
-    //       .map((a) => a.name)
-    //       .includes(name)
-    //   ) {
-    //     console.log(id, name);
-    //   }
-    // }
-    // /* Space */
-    // const serList = await Promise.all(res.data.map((project) => services.list(project.id)));
-    // console.log(
-    //   serList
-    //     .map((s, index) => ({
-    //       id: s.data[0]?.id,
-    //       project: res.data[index].attributes.name,
-    //       projectId: res.data[index].id,
-    //       name: s.data[0]?.attributes.name
-    //     }))
-    //     .filter((d) => Boolean(d.id))
-    // );
-    // const res = await people.list();
-    // console.log(
-    //   res.data
-    //     .filter((a) => a.attributes.first_name === 'Fran')
-    //     .filter((a) => a.attributes.last_name === 'Zekan')
-    //     .map((a) => ({ id: a.id, name: `${a.attributes.first_name} ${a.attributes.last_name}` }))
-    // );
+    await Promise.all(promises);
   } catch (err) {
     console.log(err);
   }
