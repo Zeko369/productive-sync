6e1d4d2629813eec56a066b858145ac3808f8480
Fran Zekan <zekan.fran369@gmail.com>
2020-07-26 21:54:52 +0200

    Pass date to gcal

--- src/gcal/lib.ts
+++ src/gcal/lib.ts
@@ -1,6 +1,6 @@
 import listEvents from './listEvents';
 import { authenticatedWrapper } from './gcal';
 
-export const getEvents = (cid: string) => {
-  return authenticatedWrapper((auth) => listEvents(auth, cid))();
+export const getEvents = (cid: string, date?: Date) => {
+  return authenticatedWrapper((auth) => listEvents(auth, cid, date))();
 };
--- src/gcal/listEvents.ts
+++ src/gcal/listEvents.ts
@@ -1,19 +1,13 @@
 import { google } from 'googleapis';
+import { toBegin, toEnd } from '../helpers/date';
 
-const listEvents = async (auth: any, cid: string, date?: Date) => {
+const listEvents = async (auth: any, cid: string, date = new Date()) => {
   const calendar = google.calendar({ version: 'v3', auth });
-
-  const dateMin = date || new Date();
-  dateMin.setUTCHours(0, 0, 0, 0);
-  const timeMin = dateMin.toISOString();
-  const dateMax = date || new Date();
-  dateMax.setUTCHours(23, 59, 60, 0);
-  const timeMax = dateMax.toISOString();
 
   const res = await calendar.events.list({
     calendarId: cid,
-    timeMin,
-    timeMax,
+    timeMin: toBegin(date).toISOString(),
+    timeMax: toEnd(date).toISOString(),
     maxResults: 100,
     singleEvents: true,
     orderBy: 'startTime'
