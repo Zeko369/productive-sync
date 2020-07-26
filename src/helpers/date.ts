845318b51dfa9a2fe5b70f12d3979110eca889bd
Fran Zekan <zekan.fran369@gmail.com>
2020-07-26 21:54:49 +0200

    Date helpers

---
+++ src/helpers/date.ts
@@ -0,0 +1,13 @@
+export const pDate = (date = new Date()) => date.toISOString().slice(0, 10);
+
+export const toBegin = (date = new Date()) => {
+  const tmpDate = new Date(date);
+  tmpDate.setUTCHours(0, 0, 0, 0);
+  return tmpDate;
+};
+
+export const toEnd = (date = new Date()) => {
+  const tmpDate = new Date(date);
+  tmpDate.setUTCHours(23, 59, 60, 0);
+  return tmpDate;
+};
